ff0fe2a39f06362af7c3429ca9250bb24ff30abd
Shuyang Sun <shuyangsun10@gmail.com>
2025-02-17 12:34:32 -0500

    Play more games.

--- othello/pytorch/NNet.py
+++ othello/pytorch/NNet.py
@@ -18,7 +18,7 @@ args = dotdict(
     {
         "lr": 0.001,
         "dropout": 0.3,
-        "epochs": 10,
+        "epochs": 100,
         "batch_size": 64,
         "cuda": torch.cuda.is_available(),
         "num_channels": 512,
