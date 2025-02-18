4969043f89389bc4a2fd5c28b529470fdd3244c5
Shuyang Sun <shuyangsun10@gmail.com>
2025-02-17 21:57:49 -0500

    Use min of sample size and batch size as training batch size.

--- xiangqi/pytorch/NNet.py
+++ xiangqi/pytorch/NNet.py
@@ -55,7 +55,7 @@ class NNetWrapper(NeuralNet):
 
             t = tqdm(range(batch_count), desc="Training Net")
             for _ in t:
-                sample_ids = np.random.randint(len(examples), size=args.batch_size)
+                sample_ids = np.random.randint(len(examples), size=batch_size)
                 boards, pis, vs = list(zip(*[examples[i] for i in sample_ids]))
                 boards = torch.FloatTensor(np.array(boards).astype(np.float64))
                 target_pis = torch.FloatTensor(np.array(pis))
